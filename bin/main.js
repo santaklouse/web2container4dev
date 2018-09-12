ce4f16670102f649c9a8053832f9b7022518c2c3
Alex Nevpryaga <alexnevpryaga@gmail.com>
2018-09-12 14:30:07 +0300

    working alfa version

--- bin/main.js
+++ bin/main.js
@@ -1,45 +1,53 @@
 #!/usr/bin/env node
+
 const argv = require('yargs').argv;
 const web2container4dev = require('../index.js');
 
-var help = false
-var dashdash = false
-var noglob = false
-var args = process.argv.slice(2).filter(function(arg) {
-    if (dashdash)
-        return !!arg
-    else if (arg === '--')
-        dashdash = true
-    else if (arg.match(/^(-+|\/)(h(elp)?|\?)$/))
-        help = true
-    else
-        return !!arg
-});
-
-if (help || args.length === 0) {
+const help = () => {
     // If they didn't ask for help, then this is not a "success"
     var log = help ? console.log : console.error;
     log('Usage: web2container4dev <command>');
     log('');
     log('  Compiles docker-compose.yaml for project and modify hosts file.');
     log('');
-    log('Commands:');
-    log('');
-    log('  build-config,      Just builds Config');
-    log('');
-    log('  run,      Build Config and run `compose run` command');
-    log('');
-    log('  exec,      Exec command inside container (see docker-compose#exec)');
+
     log('Options:');
     log('');
+    log('  -c, --command    Command to call. Can be:');
+    log('');
+    log('   add-to-hosts,      Add host to system hosts file (requires root)');
+    log('');
+    log('   build-config,      Just builds Config');
+    log('');
+    log('   run,      Build Config and run `compose run` command');
+    log('');
+    log('   exec,      Exec command inside container (see docker-compose#exec)');
+    log('');
+    log('  --params     params for command. Ex.: --params [\'param1\', \'param2\']');
+    log('');
     log('  -h, --help     Display this usage info');
-    process.exit(help ? 0 : 1);
+};
+
+if (argv.help || argv.h || argv.length === 0) {
+    help();
+    process.exit((argv.help || argv.h) ? 0 : 1);
 }
 
-if (argv.buildConfig) {
-    web2container4dev.buildConfig();
-} else if (argv.run) {
-    web2container4dev.run();
-} else if (argv.exec) {
-    web2container4dev.exec(argv.exec);
+const commands = {
+    //argv => web2container4dev
+    'build-config': 'buildConfig',
+    run: 'run',
+    exec: 'exec',
+    hostname: 'hostname',
+    'add-to-hosts': 'addDev2Hosts',
+    'run-proxy': 'runProxy'
+};
+
+const command = (argv.command || argv.c) && commands[(argv.command || argv.c)];
+if (command) {
+    web2container4dev[command].call(web2container4dev, argv.params && JSON.parse(argv.params));
+} else {
+    help();
 }
+
+
